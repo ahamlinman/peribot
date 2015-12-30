ee0eb9fbe9d292cbe1711cf04a8d79afdd770389
Alex Hamlin <alexh1995@gmail.com>
2015-12-30 02:38:50 -0500

    Add SimpleCov as a development dependency
    Add SimpleCov to the gemfile as a development dependency. Additionally,
    start SimpleCov in the spec_helper (but don't necessarily require it).

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,11 @@
+begin
+  require 'simplecov'
+  SimpleCov.start do
+    add_filter 'spec/' # Exclude RSpec files from coverage
+  end
+rescue LoadError
+  $stderr.puts 'NOTE: SimpleCov is not installed'
+end
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'peribot'
