c6e90577cdd85d2b7f9484fc3722b4e9219ad63a
Alex Hamlin <alexh1995@gmail.com>
2016-01-02 16:51:22 -0500

    Complete service base implementation and tests
    Complete the implementation of the Peribot::Services::Base#accept
    method, which asynchronously runs all command handlers within a given
    service before sending replies to the postprocessor.

--- lib/peribot/services/base.rb
+++ lib/peribot/services/base.rb
@@ -1,3 +1,5 @@
+require 'concurrent'
+
 module Peribot
   module Services
     # Base class for all Peribot services. This provides the on_message,
@@ -7,14 +9,14 @@ module Peribot
       class << self
         # Ensure that handler lists get set in subclasses, and allow them to be
         # accessible.
-        def inherited(base)
-          Peribot::Services.list << base
+        def inherited(subclass)
+          Peribot::Services.list << subclass
 
-          base.instance_variable_set :@message_handlers, []
-          base.instance_variable_set :@command_handlers, {}
-          base.instance_variable_set :@listen_handlers, {}
+          subclass.instance_variable_set :@message_handlers, []
+          subclass.instance_variable_set :@command_handlers, {}
+          subclass.instance_variable_set :@listen_handlers, {}
 
-          class << base
+          class << subclass
             attr_reader :message_handlers, :command_handlers, :listen_handlers
           end
         end
@@ -45,6 +47,140 @@ module Peribot
         def on_hear(regex, handler)
           @listen_handlers[regex] = handler
         end
+      end
+
+      # Begin processing a new message. This will cause each appropriate
+      # handler method to be called. Any messages that handler methods wish to
+      # send will be sent to the postprocessing chain after processing by the
+      # service is completed.
+      #
+      # @param message [Hash] The message to process
+      # @return [Concurrent::Promise] A promise chain for this message
+      def accept(message)
+        promise = Concurrent::Promise.fulfill []
+        promise = chain_handlers promise, message
+        promise = promise.then { |msgs| end_action msgs, message }
+
+        promise.execute
+      end
+
+      private
+
+      # Chain calls to appropriate handler methods onto the given promise.
+      #
+      # @param promise [Concurrent::Promise] The initial promise
+      # @param message [Hash] The message being processed
+      # @return [Concurrent::Promise] The promise with handlers
+      def chain_handlers(promise, message)
+        promise = chain_message_handlers promise, message
+        promise = chain_command_handlers promise, message
+        chain_listen_handlers promise, message
+      end
+
+      # Chain calls to message handlers onto a promise.
+      #
+      # @see chain_handlers
+      def chain_message_handlers(promise, message)
+        self.class.message_handlers.reduce(promise) do |prom, handler|
+          prom.then(&handler_proc(handler, message))
+        end
+      end
+
+      # Chain calls to command handlers onto a promise
+      #
+      # @see chain_handlers
+      def chain_command_handlers(promise, message)
+        self.class.command_handlers.reduce(promise) do |prom, (cmd, handler)|
+          next prom unless message['text'].match(/\A##{cmd}/)
+
+          args = message['text'].split[1..-1].join(' ')
+          args = nil if args.length == 0
+
+          prom.then(&handler_proc(handler, cmd, args, message))
+        end
+      end
+
+      # Chain calls to listen handlers onto a promise
+      #
+      # @see chain_handlers
+      def chain_listen_handlers(promise, message)
+        self.class.listen_handlers.reduce(promise) do |prom, (regex, handler)|
+          next prom unless message['text'] =~ regex
+
+          match = regex.match message['text']
+          prom.then(&handler_proc(handler, match, message))
+        end
+      end
+
+      # Get a proc that can be chained onto a promise to call a handler method.
+      #
+      # @param handler [Symbol] The handler method to call
+      # @param message [Hash] The message being processed
+      #
+      # @overload handler_proc(handler, message)
+      #   Obtain a proc to call a message handler
+      #
+      # @overload handler_proc(handler, cmd, args, message)
+      #   Obtain a proc to call a command handler
+      #   @param cmd [String] The command that was given
+      #   @param args [String] Arguments to the command
+      #
+      # @overload handler_proc(handler, match, message)
+      #   Obtain a proc to call a listen handler
+      #   @param match [MatchData] Regex match information for the message
+      #
+      # @return [Proc] A proc that will call the handler
+      def handler_proc(*args, message)
+        proc do |msgs|
+          begin
+            msgs << send(*args, message)
+          rescue => error
+            failure_action error, message
+            msgs
+          end
+        end
+      end
+
+      # Send any messages created by handlers to the postprocessing chain.
+      #
+      # @param message [Hash] The message being processed
+      def end_action(replies, original)
+        msgs = replies.flatten.reject(&:nil?)
+        msgs = convert_strings_to_replies msgs, original
+        msgs.each { |msg| Peribot::Postprocessor.instance.accept msg }
+      end
+
+      # Normalize an array containing mixed strings and messages so that it
+      # only contains message hashes.
+      #
+      # @param replies [Array] An array of replies to a message
+      # @param original [Hash] The original message
+      # @return [Array<Hash>] A normalized array of replies
+      def convert_strings_to_replies(replies, original)
+        replies.map do |reply|
+          next reply unless reply.is_a? String
+          { 'group_id' => original['group_id'], 'text' => reply }
+        end
+      end
+
+      # Handle errors in message processing by printing the error to stderr.
+      #
+      # @param error [Exception] The error that was raised
+      # @param message [Hash] The message being processed
+      def failure_action(error, message)
+        Peribot.log "#{self.class}: Error while processing message\n"\
+                    "  => message = #{message.inspect}\n"\
+                    "  => exception = #{error.inspect}\n"\
+                    "  => backtrace:\n#{format_backtrace error.backtrace}"
+      end
+
+      # Format an exception backtrace for printing to the log.
+      #
+      # @param backtrace [Array<String>] Lines of the backtrace
+      # @return [String] An indented backtrace with newlines
+      def format_backtrace(backtrace)
+        indent = 5
+        backtrace.map { |line| line.prepend(' ' * indent) }.join("\n")
       end
     end
   end
--- spec/peribot/services/base_spec.rb
+++ spec/peribot/services/base_spec.rb
@@ -36,4 +36,144 @@ describe Peribot::Services::Base do
 
     expect(subclass.listen_handlers).to include(/match/ => :test)
   end
+
+  describe '#accept' do
+    let(:base) { Peribot::Services::Base }
+    let(:message) { { 'group_id' => '1234', 'text' => '#test this' }.freeze }
+    let(:reply) { { 'group_id' => '1234', 'text' => 'Success!' } }
+    let(:postprocessor) { Peribot::Postprocessor.instance }
+
+    it 'returns a promise' do
+      subclass = Class.new(base)
+      expect(subclass.new.accept({})).to be_instance_of(Concurrent::Promise)
+    end
+
+    context 'with a message handler' do
+      let(:subclass) do
+        Class.new(base) do
+          def test_handler(*)
+            'Success!'
+          end
+          on_message :test_handler
+        end
+      end
+
+      it 'replies to any message' do
+        expect(postprocessor).to receive(:accept).with(reply)
+        subclass.new.accept(message).value
+      end
+    end
+
+    context 'with a command handler' do
+      let(:subclass) do
+        Class.new(base) do
+          def test_handler(_command, _arguments, message)
+            { 'group_id' => message['group_id'], 'text' => 'Success!' }
+          end
+          on_command :test, :test_handler
+        end
+      end
+
+      it 'replies to messages with commands' do
+        expect(postprocessor).to receive(:accept).with(reply)
+        subclass.new.accept(message).value
+      end
+
+      it 'does not reply to messages without commands' do
+        expect(postprocessor).to_not receive(:accept)
+
+        bad_msg = message.dup
+        bad_msg['text'] = 'Do not process this!'
+        subclass.new.accept(bad_msg).value
+      end
+    end
+
+    context 'with a listen handler' do
+      let(:subclass) do
+        Class.new(base) do
+          def listen_handler(_match, message)
+            { 'group_id' => message['group_id'], 'text' => 'Success!' }
+          end
+          on_hear(/this/, :listen_handler)
+        end
+      end
+
+      it 'replies to messages matching a regex' do
+        expect(postprocessor).to receive(:accept).with(reply)
+        subclass.new.accept(message).value
+      end
+
+      it 'does not reply to messages not matching the regex' do
+        expect(postprocessor).to_not receive(:accept)
+
+        bad_msg = message.dup
+        bad_msg['text'] = 'It will not match!'
+        subclass.new.accept(bad_msg).value
+      end
+    end
+
+    context 'with a handler returning nil' do
+      let(:subclass) do
+        Class.new(base) do
+          def handler(*)
+            nil
+          end
+          on_message :handler
+        end
+      end
+
+      it 'does not send a reply' do
+        expect(postprocessor).to_not receive(:accept)
+        subclass.new.accept(message).value
+      end
+    end
+
+    context 'with a handler returning multiple messages' do
+      let(:subclass) do
+        Class.new(base) do
+          def handler(*)
+            ['Success!', { 'another' => 'reply' }]
+          end
+          on_message :handler
+        end
+      end
+
+      it 'sends multiple replies' do
+        expect(postprocessor).to receive(:accept).with(reply)
+        expect(postprocessor).to receive(:accept).with('another' => 'reply')
+        subclass.new.accept(message).value
+      end
+    end
+
+    context 'with a handler that raises an error' do
+      let(:subclass) do
+        Class.new(base) do
+          def failing_handler(*)
+            fail 'This is just part of the test.'
+          end
+          on_message :failing_handler
+
+          def succeeding_handler(*)
+            'Success!'
+          end
+          on_message :succeeding_handler
+        end
+      end
+
+      it 'allows other handlers to work' do
+        # Silencing stderr only for this test
+        original_stderr = $stderr
+        $stderr = File.open(File::NULL, 'w')
+
+        expect(postprocessor).to receive(:accept).with(reply)
+        subclass.new.accept(message).value
+
+        $stderr = original_stderr
+      end
+
+      it 'logs the error to stderr' do
+        expect { subclass.new.accept(message).value }.to output.to_stderr
+      end
+    end
+  end
 end
