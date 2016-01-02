a4086a54d2b083e2473bb112af6e8ce350ea96a1
Alex Hamlin <alexh1995@gmail.com>
2016-01-02 17:20:17 -0500

    Add service dispatch method
    Create the Peribot::Services.dispatch method, which asynchronously
    dispatches a message to all services in the system.

--- lib/peribot/services.rb
+++ lib/peribot/services.rb
@@ -1,5 +1,7 @@
 require 'peribot/services/base'
 
+require 'concurrent'
+
 module Peribot
   # This module contains functionality for creating and working with services
   # in Peribot, which receive and can reply to messages from GroupMe. These
@@ -7,5 +9,21 @@ module Peribot
   module Services
     @list = []
     class << self; attr_reader :list; end
+
+    module_function
+
+    # Dispatch a message from the preprocessor to all services. This returns a
+    # future whose value will be an array of promise chains created by the
+    # services.
+    #
+    # @param message [Hash] The message to process
+    # @return [Concurrent::Future] The future used for dispatching
+    def dispatch(message)
+      Concurrent::Future.execute do
+        @list.map do |service|
+          service.new.accept message
+        end
+      end
+    end
   end
 end
