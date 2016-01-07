62d5e386541de8a10e2f1b29d500c95ffda3395b
Alex Hamlin <alexh1995@gmail.com>
2016-01-07 01:56:12 -0500

    Refactor middleware chain class
    Complete the refactoring of the middleware chain class so that chains
    are created by instantiating Peribot::Middleware::Chain rather than
    extending it. Tasks (anything that can be instantiated and has a
    process instance method) can be registered to add them to the chain.

--- lib/peribot/middleware/chain.rb
+++ lib/peribot/middleware/chain.rb
@@ -3,40 +3,31 @@ require 'singleton'
 
 module Peribot
   module Middleware
-    # This class represents the prototype for a middleware chain. A chain
-    # consists of a number of tasks followed by an end action. A message is
-    # sent through the chain using the accept method, where it is
-    # asynchronously processed by each task before being passed to the end
-    # action.
+    # This class is used to create middleware chains. A chain consists of a
+    # number of tasks followed by an end action. A message is sent through the
+    # chain using the accept method, where it is asynchronously processed by
+    # each task before being passed to the end action.
     #
     # Peribot uses middleware chains for preprocessing, postprocessing, and
-    # sending messages. New chains are created by extending this class and
-    # overriding the end_action method if necessary.
+    # sending messages. New chains are created by creating new instances of
+    # this class. End actions are provided via a block passed to
+    # Peribot::Middleware::Chain.new.
     class Chain
-      include Concurrent::Async
-      include Singleton
-
-      class << self
-        def inherited(base)
-          super
-
-          # Define the chain of task classes
-          base.instance_variable_set :@chain, []
-          class << base; attr_reader :chain; end
-
-          # Define a child Task class, which tasks intended for this chain will
-          # subclass. That is, if MyChain subclasses this class, this line will
-          # ensure that MyChain::Task is defined.
-          base.const_set :Task, Peribot::Middleware::Task.build_class(base)
-        end
+      # Create a new middleware chain. A block may be passed in order to define
+      # the end action for this chain - that is, where messages are passed once
+      # processing is completed.
+      def initialize(&end_action)
+        @end_action = end_action if block_given?
+        @tasks = []
       end
+      attr_reader :tasks
 
-      # Called to perform the ending action for this middleware chain. That is,
-      # this method is called with the final message once all tasks in the
-      # chain have processed it. Subclasses should override this method.
+      # Register a task with this middleware chain. The given task will be
+      # instantiated and used to process the message.
       #
-      # @param _message_ [Hash] The final, processed message
-      def end_action(_message_)
+      # @param task [Class] A class with a #process method taking a message
+      def register(task)
+        @tasks << task
       end
 
       # Begin processing a message using the tasks defined for this middleware
@@ -48,6 +39,8 @@ module Peribot
         promise_chain(message).execute
       end
 
+      private
+
       # Construct a promise chain for the given message. This is essentially a
       # task that gets run on a thread pool in order to process the message
       # using the middleware tasks defined for this chain.
@@ -57,7 +50,7 @@ module Peribot
       def promise_chain(message)
         promise = Concurrent::Promise.fulfill message
         promise = chain_tasks promise
-        promise = promise.then(&method(:end_action))
+        promise = promise.then(&@end_action) if @end_action
 
         promise.rescue do |e|
           Peribot.log "#{self.class}: Message processing stopped\n"\
@@ -66,8 +59,6 @@ module Peribot
         end
       end
 
-      private
-
       # (private)
       #
       # Chain the tasks for this middleware chain onto the given promise.
@@ -75,7 +66,7 @@ module Peribot
       # @param promise [Concurrent::Promise] The initial promise
       # @return [Concurrent::Promise] The promise with additional children
       def chain_tasks(promise)
-        self.class.chain.inject(promise) do |current_chain, task|
+        @tasks.reduce(promise) do |current_chain, task|
           current_chain.then { |msg| task.new.process msg }
         end
       end
