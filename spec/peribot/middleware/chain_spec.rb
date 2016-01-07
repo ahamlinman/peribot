5801206d47effe6d56b6455459164706e547dff1
Alex Hamlin <alexh1995@gmail.com>
2016-01-07 01:41:01 -0500

    Update spec for Peribot::Middleware::Chain
    As part of this refactoring effort, the usage of
    Peribot::Middleware::Chain will be changing. Instead of extending this
    class in order to create chain classes (with task subclasses created
    through complex metaprogramming), chains will be created simply by
    instantiating the class and providing an optional end action through a
    block.

--- spec/peribot/middleware/chain_spec.rb
+++ spec/peribot/middleware/chain_spec.rb
@@ -1,93 +1,65 @@
 require 'spec_helper'
 
 describe Peribot::Middleware::Chain do
-  it 'creates a chain in subclasses' do
-    subclass = Class.new Peribot::Middleware::Chain
-    expect(subclass.chain).to_not be_nil
+  it 'contains an accessible list of tasks' do
+    instance = Peribot::Middleware::Chain.new
+    expect(instance).to respond_to(:tasks)
   end
 
-  it 'creates a Task class in subclasses' do
-    subclass = Class.new Peribot::Middleware::Chain
+  describe '#register' do
+    it 'adds a task to the task list' do
+      instance = Peribot::Middleware::Chain.new
+      task = Class.new(Peribot::Middleware::Task)
+      instance.register task
 
-    expect(subclass::Task).to be_instance_of(Class)
-    expect(subclass::Task.ancestors).to include(Peribot::Middleware::Task)
-  end
-
-  it 'causes subclasses to be singletons' do
-    subclass = Class.new Peribot::Middleware::Chain
-
-    expect(subclass).to respond_to(:instance)
-    expect { subclass.new }.to raise_error(NoMethodError)
-    expect(subclass.instance).to eq(subclass.instance)
+      expect(instance.tasks).to include(task)
+    end
   end
 
   describe '#accept' do
-    it 'schedules and returns a Concurrent::Promise' do
-      chain_class = Class.new Peribot::Middleware::Chain
-      promise = chain_class.instance.accept('test' => true)
-
-      expect(promise).to be_instance_of(Concurrent::Promise)
-    end
-
-    it 'causes a promise to be executed' do
-      chain_class = Class.new(Peribot::Middleware::Chain) do
-        def end_action(message)
-          puts message.inspect
-        end
-      end
-      chain = chain_class.instance
-
-      expect { chain.accept({}).value }.to output("{}\n").to_stdout
-    end
-  end
-
-  describe '#promise_chain' do
-    before(:each) do
-      @chain = Class.new(Peribot::Middleware::Chain) do
-        def end_action(message)
-          @end_message = message
-        end
-        attr_reader :end_message
-      end
-    end
-
     it 'returns a Concurrent::Promise' do
-      chain = @chain.instance
-      promise = chain.promise_chain('test' => true)
+      instance = Peribot::Middleware::Chain.new
+      promise = instance.accept('test' => true)
 
       expect(promise).to be_instance_of(Concurrent::Promise)
     end
 
-    it 'includes the end action in the promise chain' do
-      chain = @chain.instance
-      chain.promise_chain('test' => true).execute.value
+    context 'with an end action' do
+      it 'executes the end action' do
+        chain = Peribot::Middleware::Chain.new do |msg|
+          puts msg.inspect
+        end
 
-      expect(chain.end_message).to eq('test' => true)
+        expect { chain.accept({}).value }.to output("{}\n").to_stdout
+      end
     end
 
-    it 'includes tasks in the promise chain' do
-      Class.new(@chain::Task) do
-        def process(message)
-          message['test'] = :run
-          message
+    context 'with one task' do
+      it 'executes the task' do
+        chain = Peribot::Middleware::Chain.new
+        task = Class.new(Peribot::Middleware::Task) do
+          def process(msg)
+            puts msg.inspect
+          end
         end
+        chain.register task
+
+        expect { chain.accept({}).value }.to output("{}\n").to_stdout
       end
-
-      chain = @chain.instance
-      chain.promise_chain('test' => true).execute.value
-
-      expect(chain.end_message['test']).to eq(:run)
     end
 
-    it 'causes processing failures to get logged to stderr' do
-      Class.new(@chain::Task) do
-        def process(_)
-          fail StopIteration
+    context 'with a task raising an error' do
+      it 'logs to stderr' do
+        chain = Peribot::Middleware::Chain.new
+        task = Class.new(Peribot::Middleware::Task) do
+          def process(*)
+            fail 'just testing'
+          end
         end
-      end
+        chain.register task
 
-      chain = @chain.instance
-      expect { chain.promise_chain({}).execute.value }.to output.to_stderr
+        expect { chain.accept({}).value }.to output.to_stderr
+      end
     end
   end
 end
