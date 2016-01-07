1047dcbce9677bc807a21ba9463bdf57095685ec
Alex Hamlin <alexh1995@gmail.com>
2016-01-07 02:47:35 -0500

    Update spec to initialize middleware with bot
    Update the specs for middleware chains to reflect that they should be
    initialized with a Peribot instance rather than attempting to rely on
    anything global.

--- spec/peribot/middleware/chain_spec.rb
+++ spec/peribot/middleware/chain_spec.rb
@@ -1,14 +1,15 @@
 require 'spec_helper'
 
 describe Peribot::Middleware::Chain do
+  let(:bot) { Peribot }
+  let(:instance) { Peribot::Middleware::Chain.new bot }
+
   it 'contains an accessible list of tasks' do
-    instance = Peribot::Middleware::Chain.new
     expect(instance).to respond_to(:tasks)
   end
 
   describe '#register' do
     it 'adds a task to the task list' do
-      instance = Peribot::Middleware::Chain.new
       task = Class.new(Peribot::Middleware::Task)
       instance.register task
 
@@ -18,15 +19,13 @@ describe Peribot::Middleware::Chain do
 
   describe '#accept' do
     it 'returns a Concurrent::Promise' do
-      instance = Peribot::Middleware::Chain.new
       promise = instance.accept('test' => true)
-
       expect(promise).to be_instance_of(Concurrent::Promise)
     end
 
     context 'with an end action' do
       it 'executes the end action' do
-        chain = Peribot::Middleware::Chain.new do |msg|
+        chain = Peribot::Middleware::Chain.new(bot) do |msg|
           puts msg.inspect
         end
 
@@ -36,29 +35,27 @@ describe Peribot::Middleware::Chain do
 
     context 'with one task' do
       it 'executes the task' do
-        chain = Peribot::Middleware::Chain.new
         task = Class.new(Peribot::Middleware::Task) do
           def process(msg)
             puts msg.inspect
           end
         end
-        chain.register task
+        instance.register task
 
-        expect { chain.accept({}).value }.to output("{}\n").to_stdout
+        expect { instance.accept({}).value }.to output("{}\n").to_stdout
       end
     end
 
     context 'with a task raising an error' do
       it 'logs to stderr' do
-        chain = Peribot::Middleware::Chain.new
         task = Class.new(Peribot::Middleware::Task) do
           def process(*)
             fail 'just testing'
           end
         end
-        chain.register task
+        instance.register task
 
-        expect { chain.accept({}).value }.to output.to_stderr
+        expect { instance.accept({}).value }.to output.to_stderr
       end
     end
   end
