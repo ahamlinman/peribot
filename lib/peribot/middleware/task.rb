74fe0a8470926c8cf00a80450373fa7d10801a99
Alex Hamlin <alexh1995@gmail.com>
2016-01-07 02:42:41 -0500

    Get rid of Peribot::Middleware::Task.build_class
    With the new method for creating and using middleware chains, the
    machinery to create chain-specific task classes is no longer required.

--- lib/peribot/middleware/task.rb
+++ lib/peribot/middleware/task.rb
@@ -5,24 +5,11 @@ module Peribot
     # and pass them on to other middleware tasks or other parts of the Peribot
     # message chain.
     class Task
-      # Define and return a new subclass of Peribot::Middleware::Task that will
-      # add its subclasses to chain_class.chain. In other words, this is what
-      # ensures that all task classes get added to the proper chains.
-      #
-      # @param chain_class [Class] The middleware chain class
-      def self.build_class(chain_class)
-        Class.new(self) do |cls|
-          cls.define_singleton_method :inherited do |base|
-            chain_class.chain << base
-          end
-        end
-      end
-
       # Process a message within this middleware task. This method may perform
       # one of the following actions:
       # * Return the message unchanged
       # * Return a modified message
-      # * Raise StopIteration to prevent further processing
+      # * Raise an error to prevent further processing
       #
       # @param _message_ [Hash] The message to be processed
       # @return [Hash] A message, potentially changed by this task
