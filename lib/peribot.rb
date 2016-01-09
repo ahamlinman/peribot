28b404b5882b4b366df2af978e7b04d16eb10ad1
Alex Hamlin <alexh1995@gmail.com>
2016-01-09 09:46:15 -0500

    Get rid of global middleware classes
    Remove the Pre/Postprocessor and Sender classes. These were a simple
    stopgap put in before Peribot::Bot was completed. They were extremely
    simple and not thread-safe (and thus not fit for real use in Peribot).

--- lib/peribot.rb
+++ lib/peribot.rb
@@ -4,8 +4,5 @@ end
 
 require 'peribot/bot'
 require 'peribot/middleware'
-require 'peribot/preprocessor'
-require 'peribot/postprocessor'
-require 'peribot/sender'
 require 'peribot/service'
 require 'peribot/version'
