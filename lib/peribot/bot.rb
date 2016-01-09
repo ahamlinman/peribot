7f64042eafc31dc687bd9e48436917f21f485ea7
Alex Hamlin <alexh1995@gmail.com>
2016-01-09 08:37:08 -0500

    Create Peribot::Bot class
    Create a Peribot::Bot class which includes the functionality of the
    Peribot module, but as a class that can be instantiated.
    
    FUNCTIONALITY IS STILL DUPLICATED AND NEEDS TO BE REMOVED.

--- lib/peribot/bot.rb
+++ lib/peribot/bot.rb
@@ -7,5 +7,58 @@ module Peribot
   # storage, etc. This is the single most important part of a Peribot instance,
   # as it is what connects all other components together.
   class Bot
+    include Peribot::Bot::Configuration
+    include Peribot::Bot::Stores
+
+    # Create a new Peribot instance and set up its basic configuration options.
+    def initialize(options = {})
+      setup_config_directory options[:config_directory]
+      setup_store_directory options[:store_directory]
+
+      setup_middleware_chains
+
+      @services = []
+    end
+    attr_reader :preprocessor, :postprocessor, :sender, :services
+
+    # Register a service with this Peribot instance. It will be instantiated
+    # and used to process each message that this bot receives.
+    def register(service)
+      @services << service
+    end
+
+    # A simple logging function for use by Peribot components. Outputs the
+    # given message to stderr with a "[Peribot]" prefix.
+    #
+    # @param message Text to output to stderr
+    def log(message)
+      $stderr.puts "[Peribot] #{message}"
+    end
+
+    private
+
+    # Set up preprocessing, postprocessing, and sending chains for this bot
+    # instance.
+    def setup_middleware_chains
+      @preprocessor = Peribot::Middleware::Chain.new(self) do |message|
+        dispatch message
+      end
+
+      @postprocessor = Peribot::Middleware::Chain.new(self) do |message|
+        @sender.accept message
+      end
+
+      @sender = Peribot::Middleware::Chain.new(self)
+    end
+
+    # Dispatch a message to all services in this bot instance.
+    #
+    # @param message [Hash] The message to send to services
+    def dispatch(message)
+      @services.map do |service|
+        instance = service.new self, @postprocessor
+        instance.accept message
+      end
+    end
   end
 end
---
+++ spec/peribot/bot_spec.rb
@@ -0,0 +1,90 @@
+require 'spec_helper'
+
+describe Peribot::Bot do
+  let(:instance) { Peribot::Bot.new(config_directory: '', store_directory: '') }
+
+  let(:task) do
+    Class.new(Peribot::Middleware::Task) do
+      def process(message)
+        puts message.inspect
+      end
+    end
+  end
+
+  let(:service) do
+    Class.new(Peribot::Services::Base) do
+      def handle(*)
+        { 'group_id' => '0', 'text' => 'Reply' }
+      end
+      on_message :handle
+    end
+  end
+
+  it 'has a preprocessor chain' do
+    expect(instance.preprocessor).to be_instance_of(Peribot::Middleware::Chain)
+  end
+
+  it 'has a postprocessor chain' do
+    expect(instance.postprocessor).to be_instance_of(Peribot::Middleware::Chain)
+  end
+
+  it 'has a sender chain' do
+    expect(instance.sender).to be_instance_of(Peribot::Middleware::Chain)
+  end
+
+  describe '#register' do
+    let(:service) { Class.new(Peribot::Services::Base) }
+
+    it 'adds a service to the service list' do
+      instance.register service
+      expect(instance.services).to include(service)
+    end
+  end
+
+  describe '#log' do
+    it 'logs messages to stderr with a prefix' do
+      expect { instance.log 'stuff' }.to output("[Peribot] stuff\n").to_stderr
+    end
+  end
+
+  describe '#preprocessor' do
+    let(:service) do
+      Class.new(Peribot::Services::Base) do
+        def handle(message)
+          puts message.inspect
+        end
+        on_message :handle
+      end
+    end
+
+    let(:result) { instance.preprocessor.accept({}).value.each(&:value) }
+
+    it 'forwards messages to services after processing' do
+      instance.register service
+      expect { result }.to output("{}\n").to_stdout
+    end
+  end
+
+  describe '#postprocessor' do
+    it 'forwards messages to senders after postprocessing' do
+      expect(instance.sender).to receive(:accept).with({})
+      instance.postprocessor.accept({}).value
+    end
+  end
+
+  context 'without a config_directory parameter' do
+    let(:instance) { Peribot::Bot.new(store_directory: '') }
+
+    it 'raises an error when instantiated' do
+      expect { instance }.to raise_error('No config directory defined')
+    end
+  end
+
+  context 'without a store_directory parameter' do
+    let(:instance) { Peribot::Bot.new(config_directory: '') }
+
+    it 'raises an error when instantiated' do
+      expect { instance }.to raise_error('No store directory defined')
+    end
+  end
+end
