13c906e4f055e8fb511ae400b6c2a25ecf70a333
Alex Hamlin <alexh1995@gmail.com>
2016-01-09 08:55:36 -0500

    Remove functionality from Peribot module
    Get rid of all of the functionality in the Peribot module that has been
    moved into Peribot::Bot. This also includes removal of the configuration
    classes, which represented an overly complicated way of configuring the
    global instance (now, it is simply done with keyword paramters in a
    constructor).

--- lib/peribot/configuration.rb
+++
@@ -1,9 +0,0 @@
-require 'peribot/configuration/builder'
-
-module Peribot
-  # A class to hold configuration options for a Peribot instance, such as the
-  # locations of directories for configuration and persistent store files.
-  class Configuration
-    attr_accessor :conf_directory, :store_directory
-  end
-end
