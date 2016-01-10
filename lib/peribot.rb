53fa3dab7c4a1905c32c26099c4e416a738f7bd5
Alex Hamlin <alexh1995@gmail.com>
2016-01-09 22:12:26 -0500

    Create Peribot.new function
    Implement Peribot.new, which simply forwards arguments to
    Peribot::Bot.new. This simplifies bot construction a bit and might
    provide a more consistent way to make bots.

--- lib/peribot.rb
+++ lib/peribot.rb
@@ -1,8 +1,24 @@
-# The top-level namespace for Peribot.
-module Peribot
-end
-
 require 'peribot/bot'
 require 'peribot/middleware'
 require 'peribot/service'
 require 'peribot/version'
+
+# The top-level namespace for Peribot.
+module Peribot
+  module_function
+
+  # A simple, short alias to create a new {Peribot::Bot} instance with the
+  # arguments given.
+  #
+  # @example Create a new bot
+  #   Peribot.new(
+  #     config_directory: File.expand_path('my/config/dir'),
+  #     store_directory: File.expand_path('my/store/dir')
+  #   )
+  #
+  # @see Peribot::Bot#initialize
+  # @return [Peribot::Bot]
+  def new(*args)
+    Peribot::Bot.new(*args)
+  end
+end
--- spec/peribot_spec.rb
+++ spec/peribot_spec.rb
@@ -4,4 +4,11 @@ describe Peribot do
   it 'has a version number' do
     expect(Peribot::VERSION).not_to be nil
   end
+
+  describe '.new' do
+    it 'creates a Peribot::Bot' do
+      bot = Peribot.new config_directory: '', store_directory: ''
+      expect(bot).to be_instance_of(Peribot::Bot)
+    end
+  end
 end
