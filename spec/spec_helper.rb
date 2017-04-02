0690f3df4976e4f75177104d7e98f837ddf264f7
Alex Hamlin <alex@alexhamlin.co>
2017-04-02 11:42:47 -0700

    Set up stricter timeouts for tests
    Set stricter timeouts for rspec/eventually as well as all tests in
    general. When mutation testing, this prevents infinite cases from being
    generated.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,10 @@
+RSpec.configure do |config|
+  config.around(:each) { |example| Timeout.timeout(1, &example) }
+end
+
+require 'rspec/eventually'
+Rspec::Eventually.timeout = 0.5
+
 begin
   require 'simplecov'
   SimpleCov.start do
@@ -6,8 +13,6 @@ begin
 rescue LoadError
   $stderr.puts 'NOTE: SimpleCov is not installed'
 end
-
-require 'rspec/eventually'
 
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'peribot'
