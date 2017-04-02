ddcef48c88f8a798d8ac0330d666fb4e5ec77297
Alex Hamlin <alex@alexhamlin.co>
2017-04-01 21:25:16 -0700

    Add a standard reply normalization function
    Add a standard function that reproduces the logic used in services to
    normalize replies from handler functions. This will allow that logic to
    be used by normal processors.

--- lib/peribot/util.rb
+++ lib/peribot/util.rb
@@ -4,5 +4,34 @@ require 'peribot/util/key_value_atom'
 module Peribot
   # This module provides general utility and helper classes for Peribot.
   module Util
+    module_function
+
+    # Take an array of replies, and:
+    #
+    #   * flatten out any arrays containing multiple replies
+    #   * remove any nil replies
+    #   * convert string-only replies to Peribot-formatted message hashes
+    #     (based on the original message provided)
+    #
+    # Then, yield each resulting message to the provided block.
+    #
+    # The purpose of this function is to make construction of higher-level
+    # Peribot processors easier. A higher-level processor can abstract over
+    # some details of Peribot's internal processing structure by allowing a
+    # synchronous return of nil, a single message, or an array of messages.
+    # This function provides an easy, standard way to map that back into to the
+    # lower-level callback-based world.
+    def process_replies(replies, original, &accept)
+      outputs = replies.flatten.compact.map do |reply|
+        next reply unless reply.instance_of? String
+        {
+          service: original[:service],
+          group: original[:group],
+          text: reply
+        }
+      end
+
+      outputs.each(&accept)
+    end
   end
 end
